8df119339249f849a3be8def187617a6d9a3cecc
Stefano-Trinca <ste.developer98@gmail.com>
2025-01-27 08:12:18 +0100

    chore: update package versions and enhance field validation methods

--- lib/src/core/app_form_cubit.dart
+++ lib/src/core/app_form_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../status/export.dart';
+import 'app_form_field_base.dart';
 import 'app_form_state.dart';
 
 mixin AppFormCubit<State extends AppFormState<State>> on Cubit<State> {
@@ -15,12 +16,22 @@ mixin AppFormCubit<State extends AppFormState<State>> on Cubit<State> {
     emit(state.copyWithFields(map));
   }
 
+  /// Update one field
+  ///
+  /// use the key of the field to update
+  ///
+  /// value is the new value of the field
   void fieldUpdate(String key, dynamic value) {
     emit(state.copyWithFields({
       key: state.getAppFormField(key)?.update(value),
     }));
   }
 
+  /// Clear one field
+  ///
+  /// use the key of the field to clear
+  ///
+  /// if value is provided, the field will be updated with the value
   void fieldClear(String key, [dynamic value]) {
     emit(state.copyWithFields({key: state.getAppFormField(key)?.clear(value)}));
   }
@@ -34,6 +45,30 @@ mixin AppFormCubit<State extends AppFormState<State>> on Cubit<State> {
     emit(state.copyWithFields(map));
   }
 
+  /// Validate one or more fields
+  ///
+  /// use a list of fields to validate
+  ///
+  /// returns StateStatus.valid if all fields are valid
+  StateStatus validateFields(
+      List<AppFormFieldBase<dynamic, AppFormFieldBase<dynamic, dynamic>>>
+          fields) {
+    Map<String, dynamic> map = {};
+    bool valid = true;
+    for (var field in fields) {
+      final newField = field.validate(state.inputs);
+      if (!newField.valid) {
+        valid = false;
+      }
+      map[field.key] = newField;
+    }
+    emit(state.copyWithFields(map));
+    return valid ? StateStatus.valid : StateStatus.invalid;
+  }
+
+  /// Validate all fields of the form
+  ///
+  /// returns StateStatus.valid if all fields are valid
   StateStatus validateForm() {
     Map<String, dynamic> map = {};
     bool valid = true;
